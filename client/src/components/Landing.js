e08d6ea4e8a847a59527a38c5762b1c4f8e2303c
JolantaWro <jolanta.woronowska@gmail.com>
2023-07-12 22:44:51 +0200

    change css in landing

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -5,27 +5,27 @@ import photoKrs from "../assets/img/landingPage.jpg"
 const Landing = () => {
   return (
     <>
-      <div className="landing-container">
+      <div className="container">
         
         <div className="jumbotron mt-5">
 
-          <div className="landing-menu">
-            <h1>Witaj !</h1>
-              <p>Zaloguj się i zacznij budować swoją listę,</p>
+          <div className="heading">
+            <h1 className="heading heading--x-large">Witaj !</h1>
+              <p className="heading heading--medium">Zaloguj się i zacznij budować swoją listę przedsiębiorstw,</p>
               <Link to="/login" className="btn btn-primary">Logowanie</Link>
               <Link to="/register" className="btn btn-primary ml-3">Rejestracja</Link>
-              <p>bądź sprawdź w KRS</p>
+              <p className="heading heading--medium">bądź wyszukaj aktualne dane w KRS</p>
               <Link to="/check" className="btn btn-primary">Szukaj</Link>
           </div>
 
-          <div className="landing-photo">
+          <div className="container__img">
             <img src={photoKrs} alt="workdesc"></img>
           </div>
 
         </div>
-        <div className="landing-footer">
-          <div className="landing-footer-info">
-            <h6>©2023 Created by JolantaWro</h6>
+        <div className="">
+          <div className="footer">
+            <h6 className="footer__text">©2023 Created by JolantaWro</h6>
           </div>
         </div>
       </div>
